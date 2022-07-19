50ea41eb7ef6542eed7013b224716fe1e1e6f43f
Marinela Togoe <marinelatogoe@gmail.com>
2022-07-19 19:20:43 +0200

    Added JavaScript functionality to play the songs back and forth

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev'); 
 const playBtn = document.getElementById('play'); 
 const nextBtn = document.getElementById('next'); 
+
+// Music
+const songs = [
+   {
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: '****XXXX****'
+   }, 
+   {
+    name: 'jacinto-2',
+    displayName: 'song 2',
+    artist: '****XXXX****'
+   }, 
+   {
+    name: 'jacinto-3',
+    displayName: 'song 3',
+    artist: '****XXXX****'
+   }, 
+   {
+    name: 'jacinto-4',
+    displayName: 'Song 4',
+    artist: '****XXXX****'
+   }, 
+];
+
+
 
 // Check if Playing
 let isPlaying = false;
@@ -24,3 +54,44 @@ function pauseSong() {
 
 // Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Previous Song 
+function prevSong() {
+    songIndex--;
+
+    if (songIndex < 0) {
+        songIndex = songs.length -1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+
+// Next Song 
+function nextSong() {
+    songIndex++;
+
+    if (songIndex > songs.length -1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
