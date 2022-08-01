6a655dd3b5f68a784ddc506bab7d338244256f73
Marinela Togoe <marinelatogoe@gmail.com>
2022-08-01 20:52:06 +0200

    Added functionality for the progress bar

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev'); 
 const playBtn = document.getElementById('play'); 
 const nextBtn = document.getElementById('next'); 
@@ -92,6 +94,19 @@ function nextSong() {
 // On Load - Select First Song
 loadSong(songs[songIndex]);
 
+// Update Progress Bar and Time
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const { duration, currentTime } = e.srcElement;
+        console.log(duration, currentTime);
+        // Update progress bad width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+    }
+}
+
+
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
